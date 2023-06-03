7c3130efefbfece1f112ba4dcb3b258dfc261cf4
dewiz <churkin.it@gmail.com>
2023-06-03 23:16:44 +0300

    Added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import {RegistrarionListComponent} from './registrarion-list/registrarion-list.c
 import {UserDetailComponent} from './user-detail/user-detail.component';
 import {NgToastModule} from "ng-angular-popup";
 import {NgConfirmModule} from "ng-confirm-box";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import {NgConfirmModule} from "ng-confirm-box";
     BrowserModule,
     BrowserAnimationsModule,
     CreateRegistrationComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
     NgConfirmModule,
     NgToastModule,
     RegistrarionListComponent,
