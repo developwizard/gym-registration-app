a86f7811a2272438dfff935fffe64dd084494243
dewiz <churkin.it@gmail.com>
2023-06-05 01:19:09 +0300

    Added registration form reactive approach code connection

--- src/app/create-registrarion/create-registration.component.ts
+++ src/app/create-registrarion/create-registration.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
@@ -7,6 +7,8 @@ import {CommonModule} from "@angular/common";
 import {MatSelectModule} from "@angular/material/select";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-create-registrarion',
@@ -15,19 +17,21 @@ import {MatNativeDateModule} from "@angular/material/core";
   standalone: true,
   imports: [
     CommonModule,
+    MatButtonModule,
     MatDatepickerModule,
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
     MatNativeDateModule,
     MatRadioModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule
   ]
 })
-export class CreateRegistrationComponent {
-  public packages = ["Monthly", "Quarterly", "Yearly"];
-  public genders = ["Male", "Female"];
-  public importantList: string[] = [
+export class CreateRegistrationComponent implements OnInit {
+  packages = ["Monthly", "Quarterly", "Yearly"];
+  genders = ["Male", "Female"];
+  importantList: string[] = [
     "Toxic Fat Reduction",
     "Energy and Endurance",
     "Building Lean Muscle",
@@ -35,4 +39,31 @@ export class CreateRegistrationComponent {
     "Sugar Craving Body",
     "Fitness"
   ];
+  registrationForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      mobile: [''],
+      weight: [''],
+      height: [''],
+      bmi: [''],
+      bmiResult: [''],
+      gender: [''],
+      requireTrainer: [''],
+      package: [''],
+      important: [''],
+      haveGymBefore: [''],
+      enquiryDate: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+  }
 }
