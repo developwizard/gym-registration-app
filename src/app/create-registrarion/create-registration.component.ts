6533248daac07ae5373f2dfec6e24c2918bc37be
dewiz <churkin.it@gmail.com>
2023-06-05 00:52:48 +0300

    Added registration template

--- src/app/create-registrarion/create-registration.component.ts
+++ src/app/create-registrarion/create-registration.component.ts
@@ -1,4 +1,12 @@
 import {Component} from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatRadioModule} from "@angular/material/radio";
+import {CommonModule} from "@angular/common";
+import {MatSelectModule} from "@angular/material/select";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @Component({
   selector: 'app-create-registrarion',
@@ -6,9 +14,25 @@ import {Component} from '@angular/core';
   styleUrls: ['./create-registration.component.css'],
   standalone: true,
   imports: [
-
+    CommonModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule
   ]
 })
 export class CreateRegistrationComponent {
-
+  public packages = ["Monthly", "Quarterly", "Yearly"];
+  public genders = ["Male", "Female"];
+  public importantList: string[] = [
+    "Toxic Fat Reduction",
+    "Energy and Endurance",
+    "Building Lean Muscle",
+    "Healthier Digestive System",
+    "Sugar Craving Body",
+    "Fitness"
+  ];
 }
